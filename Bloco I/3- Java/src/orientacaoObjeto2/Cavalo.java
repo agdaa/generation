38f551a94d3da3d8f3ea0ba8f30d3a1c87760294
Agda <agda.camilla@gmail.com>
2021-06-10 00:30:53 -0300

    Exercícios de Herança

---
+++ Bloco I/3- Java/src/orientacaoObjeto2/Cavalo.java
@@ -0,0 +1,12 @@
+package orientacaoObjeto2;
+
+public class Cavalo extends Animal {
+
+	public Cavalo(String nome, int idade) {
+		super(nome, idade);
+	}
+	
+	public void correr() {
+		
+	}
+}
---
+++ Bloco I/3- Java/src/orientacaoObjeto2/Preguica.java
@@ -0,0 +1,13 @@
+package orientacaoObjeto2;
+
+public class Preguica extends Animal {
+
+	public Preguica(String nome, int idade) {
+		super(nome, idade);
+	}
+	
+	public void subirArvore () {
+		
+	}
+
+}
